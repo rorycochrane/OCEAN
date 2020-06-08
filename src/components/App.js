7b4a3c8f442461c69fa5567e909a3985e0910d59
Rory Cochrane <rory.m.cochrane@gmail.com>
2020-06-07 22:47:55 -0700

    add a scroll bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import QuestionList from './QuestionList';
 import ScoreDetail from './ScoreDetail';
+import { Segment } from 'semantic-ui-react';
 
 const App = () => {
   console.log('inside App');
   return (
     <div className="ui container grid">
       <div className="ui row">
-        <div className="column ten wide">
-          <QuestionList />
-        </div>
+        <Segment style={{overflow: 'auto', maxHeight: 500 }}>
+          <div className="column ten wide">
+            <QuestionList />
+          </div>
+        </Segment>
         <div className="column eight wide">
           <ScoreDetail />
         </div>
