1cfd6412335e25c6b40066fd03a96156c06606ab
Rory Cochrane <rory.m.cochrane@gmail.com>
2020-06-14 19:27:28 -0700

    remove unused reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,24 +1,5 @@
 import { combineReducers } from 'redux';
 
-// const songsReducer = () => {
-//   console.log('songs reducer called');
-//   return [
-//     { title: 'No Scrubs', duration: '4:05' },
-//     { title: 'Macarena', duration: '2:30' },
-//     { title: 'All Star', duration: '3:15' },
-//     { title: 'I Want it That Way', duration: '1:45' }
-//   ];
-// };
-
-// const selectedSongReducer = (selectedSong = null, action) => {
-//     console.log('Select Song reducer called');
-//     if (action.type === 'SONG_SELECTED') {
-//     return action.payload;
-//   }
-
-//   return selectedSong;
-// };
-
 const questionsReducer = () => {
     console.log('Scores reducer called');
     return [
